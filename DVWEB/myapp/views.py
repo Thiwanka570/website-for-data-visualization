172cb64fc94cc97819c62d530c299d58b203582d
Thiwanka Madushan <76912747+Thiwanka570@users.noreply.github.com>
2023-10-02 00:58:36 +0530

    bsic complete

--- DVWEB/myapp/views.py
+++ DVWEB/myapp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
-# Create your views here.
+
+file_path = os.path.join(".", "myapp", "dataset", "sample.csv")
+try:
+    dataset = pd.read_csv(file_path)
+    column_names=dataset.columns
+    column_namelist=dataset.columns.to_list()   
+except FileNotFoundError:
+    print( "CSV file not found")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+
+
+def read_columns(data):
+    columnNames=data.columns
+    return columnNames
+    
+def genarate_Graps(columns,dataset):
+    x_column=columns[1]
+    y_column=columns[2]
+    
+    x_data=dataset[x_column]
+    y_data=dataset[y_column]
+    
+    plt.plot(x_data, y_data)
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
+    plt.title(f'Plot of {x_column} vs. {y_column}')
+    plt.grid(True)
+    
+    plt.savefig('sample1.jpg')
+    
+genarate_Graps(column_namelist,dataset)
+
+def home(request):
+    columnNames = read_columns(dataset)
+    return render(request,'home.html',{'columns':columnNames})
